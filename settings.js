40b45a1aafac49d04366e5b1542ddc615237a42d
apschaut1 <aaron@theimageshoppe.com>
2017-08-15 11:38:43 -0400

    update settings

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://protected-earth-58800.herokuapp.com';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "info@essencerestaurants.com";
+exports.senderName = "Essence Restaurants Group";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = 1558826;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = null;
+exports.templateId = 8921ff2c-d365-4ee6-9e80-94d6de3242de;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = "admin@example.com";
+exports.notificationEmail = "aaron@theimageshoppe.com";
